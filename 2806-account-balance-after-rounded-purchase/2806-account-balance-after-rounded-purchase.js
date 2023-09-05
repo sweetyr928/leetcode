13d19fca7ad131c8c30691b29e66109c78533d91
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-09-05 11:13:30 +0900

    Time: 49 ms (64.91%), Space: 41.7 MB (70.76%) - LeetHub

---
+++ 2806-account-balance-after-rounded-purchase/2806-account-balance-after-rounded-purchase.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} purchaseAmount
+ * @return {number}
+ */
+var accountBalanceAfterPurchase = function(purchaseAmount) {
+    let rem = purchaseAmount % 10;
+    if (rem < 5)
+        purchaseAmount = purchaseAmount - rem;
+    else
+        purchaseAmount = purchaseAmount + (10 - rem);
+
+    return 100 - purchaseAmount;
+};
